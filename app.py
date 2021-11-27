b89f7fb5444242d975e637ae0116157bcb4b4cb0
Ziyad MBP <ziyad.dev@gmail.com>
2021-11-27 15:44:00 +0100

    code cleaning + datetime series improvement + title update

--- app.py
+++ app.py
@@ -1,49 +1,46 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from datetime import datetime
+from datetime import date, datetime
+
 import dash
-# import dash_core_components as dcc
-from dash import dcc
-# import dash_html_components as html
-from dash import html
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import pandas as pd
+# import dash_html_components as html
+# import dash_core_components as dcc
+from dash import dcc, html
 
 app = dash.Dash(__name__)
 
 df = pd.read_csv('data_25_nov_virgule.csv')
 
-# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
 #fig = px.scatter(df, x="gdp per capita", y="life expectancy",
 
 unixtime = df.unixtime
 
-date = df.date + " " + df.time
+date_dfirst = pd.to_datetime(unixtime, dayfirst=True, yearfirst=False, unit="s")
 
-print(date)
+print(date_dfirst)
 
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(x=date, y=df.condensing_temp, name='Condensing Temp',
+fig.add_trace(go.Scatter(x=date_dfirst, y=df.condensing_temp, name='Condensing Temp',
                          line=dict(color='firebrick', width=1)))
-fig.add_trace(go.Scatter(x=date, y=df.liquid_temp, name = 'Liquid Temp',
+fig.add_trace(go.Scatter(x=date_dfirst, y=df.liquid_temp, name = 'Liquid Temp',
                          line=dict(color='orange', width=1)))
-fig.add_trace(go.Scatter(x=date, y=df.subcooling, name='Subcooling',
-                         line=dict(color='orange', width=1) # dash options include 'dash', 'dot', and 'dashdot'
-                         ))
+fig.add_trace(go.Scatter(x=date_dfirst, y=df.subcooling, name='Subcooling',
+                         line=dict(color='orange', width=1)))
 
-fig.add_trace(go.Scatter(x=date, y=df.evaporating_temp, name='Evaporating Temp',
+fig.add_trace(go.Scatter(x=date_dfirst, y=df.evaporating_temp, name='Evaporating Temp',
                          line = dict(color='royalblue', width=1)))
-fig.add_trace(go.Scatter(x=date, y=df.suction_temp, name='Suction Temp',
+fig.add_trace(go.Scatter(x=date_dfirst, y=df.suction_temp, name='Suction Temp',
                          line = dict(color='firebrick', width=1)))
-fig.add_trace(go.Scatter(x=date, y=df.superheat, name='Superheat',
+fig.add_trace(go.Scatter(x=date_dfirst, y=df.superheat, name='Superheat',
                          line=dict(color='darkorchid', width=1)))
 
 # Edit the layout
-fig.update_layout(title='Daikin Chiller capture',
+fig.update_layout(title='Daikin Chiller Trend',
                    xaxis_title='Time',
                    yaxis_title='Temperature (degrees C)')
 
@@ -51,7 +48,6 @@ fig.update_layout(title='Daikin Chiller capture',
 #                  #size="population", color="continent", hover_name="country",
 #                  #log_x=True #size_max=60)
 
-fig.update_xaxes(tickformat="%D-%m-%Y")
 fig.update_xaxes(rangeslider_visible=True)
 fig.show()
 
